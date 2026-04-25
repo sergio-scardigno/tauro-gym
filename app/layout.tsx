b8ea7e9d58e022510fc5b171c5155aa63734cfc7
Sergio <sergioscardigno82@gmail.com>
2026-04-24 21:56:55 -0300

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gaId = "G-7ZE7R2GTHB";
+  const gaInlineScript = `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${gaId}');`;
+
   const gtmId = "GTM-MJ2HRP27";
   const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -37,6 +43,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
+        <script dangerouslySetInnerHTML={{ __html: gaInlineScript }} />
+        {/* End Google tag (gtag.js) */}
       </head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
